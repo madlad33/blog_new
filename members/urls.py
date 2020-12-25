1565934f1f73c3527250dc5e65d9ec68e0d99a4b
Tanmay <tanmay.milky33@gmail.com>
2020-12-25 18:56:54 +0530

    Added edit profile to navbar and created form

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView,UserEditView
 
 urlpatterns = [
     path('/register',UserRegisterView.as_view(),name='register'),
+    path('/editprofile',UserEditView.as_view(),name='user_edit'),
 
 
     ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.urls import reverse_lazy
 # Create your views here.
 from .forms import SignUpForm
@@ -10,3 +10,9 @@ class UserRegisterView(generic.CreateView):
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
 
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = 'home'
+    def get_object(self, queryset=None):
+        return self.request.user
