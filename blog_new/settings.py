6506b54f6f3ff60a7240060e1190ebf9aae6ad51
Tanmay <tanmay.milky33@gmail.com>
2021-03-08 20:40:17 +0530

    Deployment settings

--- blog_new/settings.py
+++ blog_new/settings.py
@@ -119,13 +119,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'static'),
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=(
+#     os.path.join(BASE_DIR,'static'),
+# )
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
 MEDIA_URL ='/media/'
 MEDIA_ROUTE=os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- blog_new/wsgi.py
+++ blog_new/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_new.settings')
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(PROJECT_ROOT, 'staticfiles'))
